c67ebbd22a6af0ef8294c6fdf873ac2513cd0ea4
QuantumRac <cynthiayao11@outlook.com>
2025-09-26 12:26:40 -0400

    Added Vercel speed insights to main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,8 +42,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <SpeedInsights />
+          
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
